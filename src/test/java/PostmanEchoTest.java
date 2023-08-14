d0867b036bf881b96823e42902ab69b87c5830f5
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-08-14 14:46:31 +0300

    Built success

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("date", equalTo("The 14th of August") )
+                .body("data", equalTo("The 14th of August") )
         ;
     }
 }
